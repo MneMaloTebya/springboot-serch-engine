d2ac63f3e3f7c2cdeba3cf9dfc7aa880543394ac
Alexander <LipatovAlexander93@yandex.ru>
2022-11-04 17:50:57 +0300

    added page and site entity. but i have error in unique key in path colmn

---
+++ src/main/java/searchengine/model/entity/Page.java
@@ -0,0 +1,30 @@
+package searchengine.model.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "page")
+@Data
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @Column(columnDefinition = "TEXT", nullable = false, unique = true)
+    private String path;
+
+    @Column(nullable = false)
+    private int code;
+
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+
+}
---
+++ src/main/java/searchengine/model/entity/Site.java
@@ -0,0 +1,36 @@
+package searchengine.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "site")
+@Data
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')", nullable = false)
+    private StatusType statusType;
+
+    @Column(name = "status_time", columnDefinition = "DATETIME(6)", nullable = false)
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String url;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "site", cascade = CascadeType.ALL)
+    private List<Page> pages;
+}
