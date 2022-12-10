893ad7447aa5e050c075808c4466bc9a09967d8d
Alexander <LipatovAlexander93@yandex.ru>
2022-12-10 10:52:11 +0300

    added TestLuceneMorphology

--- src/main/java/searchengine/TestLuceneMorphology.java
+++ src/main/java/searchengine/TestLuceneMorphology.java
@@ -2,14 +2,29 @@ package searchengine;
 
 import org.apache.lucene.morphology.LuceneMorphology;
 import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+import searchengine.services.lemmatizer.LemmaFinder;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestLuceneMorphology {
     public static void main(String[] args) throws IOException {
-        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
-        List<String> wordBaseForms = luceneMorph.getNormalForms("леса");
-        wordBaseForms.forEach(System.out::println);
+
+//        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
+//        List<String> wordBaseForms = luceneMorph.getNormalForms("леса");
+//        wordBaseForms.forEach(System.out::println);
+
+
+        Map<String, Integer> lemmas = new HashMap<>();
+        String text = "Повторное появление леопарда в Осетии позволяет предположить,\n" +
+                "что леопард постоянно обитает в некоторых районах Северного\n" +
+                "Кавказа. Или вообще чикибряк";
+        LemmaFinder lemmaFinder = LemmaFinder.getInstance();
+        lemmas = lemmaFinder.collectLemmas(text);
+        System.out.println(lemmas.toString());
+
+
     }
 }
